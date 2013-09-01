c5ceb3c9716535433a1bacd26e571c39ae35d436
Will Fuqua <wafuqua@gmail.com>
2013-09-01 16:32:34 -0400

    Add initial project layout for clojureclr engine

---
+++ ScriptCs.Engine.Clojure/ClojureScriptEngine.cs
@@ -0,0 +1,42 @@
+﻿using ScriptCs.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptCs.Engine.Clojure
+{
+    public class ClojureScriptEngine : IScriptEngine
+    {
+        public string BaseDirectory
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string FileName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public ScriptResult Execute(string code, string[] scriptArgs, IEnumerable<string> references, 
+            IEnumerable<string> namespaces, ScriptPackSession scriptPackSession)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
